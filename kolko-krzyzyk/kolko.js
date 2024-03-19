54a0f52d2b35f401d5d8d1475c6e165ed42a9bb6
Kasiaglocka <kasia.glocka@gmail.com>
2024-03-19 20:42:21 +0100

    dodanie funkcji- aktualny gracz

--- kolko-krzyzyk/kolko.js
+++ kolko-krzyzyk/kolko.js
@@ -5,10 +5,15 @@ for (let poleGry of pola) {
     poleGry.addEventListener('click', function(event) { 
         console.log(event)
         const kliknietePole = event.target
-        const polePrzedstawiajaceJakiZnakJestAktualny = document.getElementById("aktualnyGracz")
-        const aktualnyZnak = polePrzedstawiajaceJakiZnakJestAktualny.innerText
-        kliknietePole.innerText = aktualnyZnak
-        zmianaZnaku()
+        if (kliknietePole.innerText == ""){
+            const polePrzedstawiajaceJakiZnakJestAktualny = document.getElementById("aktualnyGracz")
+            const aktualnyZnak = polePrzedstawiajaceJakiZnakJestAktualny.innerText
+            kliknietePole.innerText = aktualnyZnak
+            sprawdzenieWyniku(aktualnyZnak)
+            zmianaZnaku()
+        }else{
+            alert("To pole jest już zajęte!")
+        }
      });
 }
 
@@ -18,6 +23,34 @@ function zmianaZnaku() {
         polePrzedstawiajaceJakiZnakJestAktualny.innerText = "X"
     }else{
         polePrzedstawiajaceJakiZnakJestAktualny.innerText = "O"
+    }
+}
+
+function sprawdzenieWyniku(aktualnyZnak) {
+    const pola = document.getElementsByClassName("kwadrat")
+    if (pola[0].innerText == aktualnyZnak && pola[1].innerText == aktualnyZnak && pola[2].innerText == aktualnyZnak){
+        alert("Gratulacje!!! Wygrał: " + aktualnyZnak)
+    }
+    if (pola[0].innerText == aktualnyZnak && pola[3].innerText == aktualnyZnak && pola[6].innerText == aktualnyZnak){
+        alert("Gratulacje!!! Wygrał: " + aktualnyZnak)
+    }
+    if (pola[3].innerText == aktualnyZnak && pola[4].innerText == aktualnyZnak && pola[5].innerText == aktualnyZnak){
+        alert("Gratulacje!!! Wygrał: " + aktualnyZnak)
+    }
+    if (pola[6].innerText == aktualnyZnak && pola[7].innerText == aktualnyZnak && pola[8].innerText == aktualnyZnak){
+        alert("Gratulacje!!! Wygrał: " + aktualnyZnak)
+    }
+    if (pola[1].innerText == aktualnyZnak && pola[4].innerText == aktualnyZnak && pola[7].innerText == aktualnyZnak){
+        alert("Gratulacje!!! Wygrał: " + aktualnyZnak)
+    }
+    if (pola[2].innerText == aktualnyZnak && pola[5].innerText == aktualnyZnak && pola[8].innerText == aktualnyZnak){
+        alert("Gratulacje!!! Wygrał: " + aktualnyZnak)
+    }
+    if (pola[0].innerText == aktualnyZnak && pola[4].innerText == aktualnyZnak && pola[8].innerText == aktualnyZnak){
+        alert("Gratulacje!!! Wygrał: " + aktualnyZnak)
+    }
+    if (pola[6].innerText == aktualnyZnak && pola[4].innerText == aktualnyZnak && pola[2].innerText == aktualnyZnak){
+        alert("Gratulacje!!! Wygrał: " + aktualnyZnak)
     }
 }
 
